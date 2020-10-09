d1dbb51c43694f664847c1e4a7f6157641760cd6
fps.io <mista.tapas@gmx.net>
2020-10-09 07:31:24 +0200

    Add a user environment to run the arduino-ide

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSUserEnv {
+  name = "simple-x11-env";
+  targetPkgs = pkgs: (with pkgs;
+    [ udev
+      alsaLib
+      fontconfig
+      zlib
+      udev
+      gtk2
+      glib
+      pango
+      cairo
+      gdk_pixbuf
+    ]) ++ (with pkgs.xorg;
+    [ libX11
+      libXcursor
+      libXrandr
+      libXext
+      libXtst
+      libXi
+      libXft
+      libXxf86vm
+      libSM
+    ]);
+  multiPkgs = pkgs: (with pkgs;
+    [ udev
+      alsaLib
+      fontconfig
+      zlib
+    ]);
+  runScript = "bash";
+}).env
+
