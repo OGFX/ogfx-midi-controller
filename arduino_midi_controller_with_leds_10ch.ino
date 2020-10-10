27e534451a6a922a81fcc3f951878e59d65fd45f
fps.io <mista.tapas@gmx.net>
2020-10-10 12:56:05 +0200

    clean up the whole code...
    - receive midi CCs and scale LED brightness
    - fix CC sending
    - fix timing 
    - other things

--- arduino_midi_controller_with_leds_10ch.ino
+++ arduino_midi_controller_with_leds_10ch.ino
@@ -7,7 +7,6 @@ const uint8_t numberOfButtons = 10;
 
 // LEDs
 const int ledPinBase = 14;
-const uint8_t numberOfLEDs = 10;
 
 // Touch INs
 const int touchInPinBase = 10;
@@ -15,30 +14,25 @@ const uint8_t numberOfTouch = 2;
 
 // An array of ints to remember the button states from the last loop
 // so we can detect state transitions
-int lastButtonState[numberOfButtons];// = { LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW };
+int lastButtonState[numberOfButtons];
 
-int isButtonProgramChange[numberOfButtons] = { 1 1 1 1 1 0 0 0 0 };
-
-int isButtonMomentary[numberOfButtons] = { 0 0 0 0 0 0 0 1 1 1 };
-
-// An array of ints to remember the last CC we sent, so we can send 
-// the corresponding different one on the next press
-int lastMidiState[numberOfButtons];// = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int ccState[numberOfButtons];
 
 // The "active" state is the pressed down state. This
 // might be different for different buttons..
 int activeButtonState[numberOfButtons];
 
-const int num_an_avg = 50;
+const int analog_in_window_size = 500;
+const int analog_in_delta_t_us = 10;
 int avg_index = 0;
 
-float an1 = 0;
-float an2 = 0;
+float analog_in1 = 0;
+float analog_in2 = 0;
 
-float an1_old = 0;
-float an2_old = 0;
+float analog_in1_old = 0;
+float analog_in2_old = 0;
 
-float an_thresh = 4.0;
+float analog_in_thresh = 4.0;
 
 const float t0 = 0.1;
 
@@ -54,32 +48,70 @@ void setup()
   {
     pinMode(buttonPinBase + button, INPUT_PULLUP);
     lastButtonState[button] = LOW;
-    lastMidiState[button] = 0;
 
     activeButtonState[button] = LOW;
   }
-  
-  for (int button = 0; button < numberOfLEDs; ++button)
+
+  for (int led = 0; led < numberOfButtons; ++led)
   {
-    pinMode(ledPinBase + button, OUTPUT);
-    digitalWrite(ledPinBase + button, HIGH);
-  }
-  
-  delay(500);
-  
-  for (int button = 0; button < numberOfLEDs; ++button)
-  {
-    digitalWrite(ledPinBase + button, LOW);
+    ccState[led] = LOW;
+    pinMode(ledPinBase + led, OUTPUT);
   }
 
+  for (int index = 0; index < 3; ++index)
+  {
+    for (int button = 0; button < numberOfButtons; ++button)
+    {
+      digitalWrite(ledPinBase + button, HIGH);
+      delay(50);
+    }
+    
+    for (int button = 0; button < numberOfButtons; ++button)
+    {
+      digitalWrite(ledPinBase + button, LOW);
+      delay(50);
+    }
+  }
+  
   pinMode(A10, INPUT);
   pinMode(A11, INPUT);
 }
 
 void loop() {
+  /* MIDI IN */
+  midiEventPacket_t in_packet;
+  
+  do {
+    in_packet = MidiUSB.read();
+    if (in_packet.header == 0x0B) {
+      int channel = in_packet.byte1 & 0x7F;
+      int controller = in_packet.byte2;
+
+      if (controller > numberOfButtons) {
+        continue;
+      }
+      
+      int value = in_packet.byte3;
+
+      if (value != 0) {
+        ccState[controller] = value;
+      } else {
+        ccState[controller] = value;
+      }
+    }
+  } while (in_packet.header != 0);
+  
   // Serial.write(".\n");
+
+  /* PROCESS BUTTONS */
   for (int button = 0; button < numberOfButtons; ++button)
   {
+    if (ccState[button] != 0) {
+      // digitalWrite(ledPinBase + button, HIGH);
+    } else {
+      // digitalWrite(ledPinBase + button, LOW);
+    }
+    
     int state = digitalRead(buttonPinBase + button);
 
     // Detect state transitions    
@@ -88,52 +120,57 @@ void loop() {
       // Going from low to high (on push)
       if (state == activeButtonState[button]) 
       {
-        // Check the last CC value we sent so we can send the right one now
-        if (lastMidiState[button] == 0)
-        {
-          midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | button), 0, 127};
-          MidiUSB.sendMIDI(packet);
-          lastMidiState[button] = 127;
-          digitalWrite(ledPinBase + button, HIGH);
-        }
-        else
-        {
-          midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | button), 0, 0};
-          MidiUSB.sendMIDI(packet);
-          lastMidiState[button] = 0;
-          digitalWrite(ledPinBase + button, LOW);
-        }
-        MidiUSB.flush();
-      } 
+        midiEventPacket_t packet = {0x0B, 0xB0, (uint8_t)button, 127};
+        MidiUSB.sendMIDI(packet);
+        // digitalWrite(ledPinBase + button, HIGH);
+      }
+      else
+      {
+        midiEventPacket_t packet = {0x0B, 0xB0, (uint8_t)button, 0};
+        MidiUSB.sendMIDI(packet);
+        // digitalWrite(ledPinBase + button, LOW);
+      }
     }
 
     // Store away the current state for the transition detection
     lastButtonState[button] = state;
   }
 
-  an1 = 0;
-  an2 = 0;
-  for (int index = 0; index < num_an_avg; ++index) {
+  /* READ EXPRESSION PEDALS AND FADE LEDS */
+  analog_in1 = 0;
+  analog_in2 = 0;
+  for (int index = 0; index < analog_in_window_size; ++index) 
+  {
+    analog_in1 += (1.0 / analog_in_window_size) * (float)analogRead(A10);
+    analog_in2 += (1.0 / analog_in_window_size) * (float)analogRead(A11);
 
-    an1 += (1.0 / num_an_avg) * (float)analogRead(A10);
-    an2 += (1.0 / num_an_avg) * (float)analogRead(A11);
-
-    delayMicroseconds(50);
+    for (int led = 0; led < numberOfButtons; ++led) {
+      if (random(0,127) < ccState[led] || lastButtonState[led] == activeButtonState[led]) {
+        digitalWrite(ledPinBase + led, HIGH);
+      } else {
+        digitalWrite(ledPinBase + led, LOW);
+      }
+    }
+    
+    delayMicroseconds(analog_in_delta_t_us);
   }
 
   // Serial.print(an1); Serial.print("\t"); Serial.print(an2); Serial.print("\n");
 
-  if (abs(an1_old - an1) > an_thresh) {
+  if (abs(analog_in1_old - analog_in1) > analog_in_thresh) {
     // Serial.print((127.0/an_max)*an1); Serial.write("\t "); Serial.print((127.0/an_max)*an2); Serial.write("\n");
-    midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | 0), 0, min(127, floor((127.0/an_max)*an1))};
+    midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | 0), min(127, floor((127.0/an_max)*analog_in1))};
     MidiUSB.sendMIDI(packet);
-    an1_old = an1;
+    analog_in1_old = analog_in1;
   }
   
-  if (abs(an2_old - an2) > an_thresh) {
+  if (abs(analog_in2_old - analog_in2) > analog_in_thresh) {
     // Serial.print((127.0/an_max)*an1); Serial.write("\t "); Serial.print((127.0/an_max)*an2); Serial.write("\n");
-    midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | 1), 0, min(127, floor((127.0/an_max)*an2))};
+    midiEventPacket_t packet = {0x0B, (uint8_t)(0xB0 | 1), min(127, floor((127.0/an_max)*analog_in2))};
     MidiUSB.sendMIDI(packet);
-    an2_old = an2;
+    analog_in2_old = analog_in2;
   }
+  
+  MidiUSB.flush();
+  // delayMicroseconds(20000);
 }
